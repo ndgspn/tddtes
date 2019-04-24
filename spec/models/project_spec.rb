d93114765bd31022eecd9010548cc2e5b5f42aa6
ndgspn <nandangsopyan08@gmail.com>
2019-04-24 13:43:06 +0700

    refactor

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,10 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Project do
+  let(:project) { Project.new }
+  let(:task) { Task.new }
+
+  it "considers a project with no tasks to be done" do
+    expect(project).to be_done
+  end
+
   it "knows that a project with an incomplete task is not done" do
-    project     = Project.new
-    task        = Task.new
     project.tasks << task
-    expect(project.done?).to be_falsy
+    expect(project).not_to be_done
   end
 end
