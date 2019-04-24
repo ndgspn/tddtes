411cf76db53aa28e8a275c5e441b3e4ea405a67d
ndgspn <nandangsopyan08@gmail.com>
2019-04-24 13:30:26 +0700

    added Task models and modified project class

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Project do
-  it "considers a project with no task to be done" do
+  it "knows that a project with an incomplete task is not done" do
     project     = Project.new
-    expect(project.done?).to be_truthy
+    task        = Task.new
+    project.tasks << task
+    expect(project.done?).to be_falsy
   end
 end
